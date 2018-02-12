015770775401144db74d05f4ebd6d571947057d2
ShangMing Cherng <night1300013@gmail.com>
2018-02-11 21:33:31 -0800

    add get_me method and @me_hash to store the user info in hash

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
@@ -12,5 +13,14 @@ class Kele
     response = self.class.post(@api_url, body: body)
     raise 'Invalid email or password.' if response.code != 200
     @auth_token = response['auth_token']
+  end
+
+  def get_me
+    url = 'https://www.bloc.io/api/v1/users/me'
+    headers = {
+      "authorization" => @auth_token
+    }
+    response = self.class.get(url, headers: headers)
+    @me_hash = JSON.parse(response.body)
   end
 end
