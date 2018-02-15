376f0feb25303ecc8994faaa4500c3b703124464
ShangMing Cherng <night1300013@gmail.com>
2018-02-14 23:42:35 -0800

    update_submission completed

--- lib/kele/submission.rb
+++ lib/kele/submission.rb
@@ -12,6 +12,20 @@ module Submission
       "enrollment_id": @me_hash["current_enrollment"]["id"],
     }
     response = self.class.post(url, body: body, headers: headers)
-    puts response
+  end
+
+  def update_submission(id, checkpoint_id, assignment_branch, assignment_commit_link, comment)
+    url = "#{api_url}/checkpoint_submissions/#{id}"
+    headers = {
+      "authorization" => @auth_token
+    }
+    body = {
+      "assignment_branch": assignment_branch,
+      "assignment_commit_link": assignment_commit_link,
+      "checkpoint_id": checkpoint_id,
+      "comment": comment,
+      "enrollment_id": @me_hash["current_enrollment"]["id"],
+    }
+    response = self.class.put(url, body: body, headers: headers)
   end
 end
